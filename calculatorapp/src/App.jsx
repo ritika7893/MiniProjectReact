2f0b407e651851f1de36ad0bbc38e4b7c1365ccb
ritika7893 <ritikakumari87344@gmail.com>
2025-07-21 11:08:38 +0530

    Simple Calcualtor Application

--- calculatorapp/src/App.jsx
+++ calculatorapp/src/App.jsx
@@ -15,16 +15,18 @@ function App() {
   }
   return (
     <>
-      <input type="number" onChange={val1} />
-      <br />
-      <input type="number" onChange={val2} /><br /> <br />
-      <button onClick={add => { setResult(num1 + num2) }}>Add</button>
-      <button onClick={sub => { setResult(num1 - num2) }}>Sub</button>
-      <button onClick={mul => { setResult(num1 * num2) }}>Mul</button>
-      <button onClick={div => { setResult(num1 / num2) }}>Divide</button>
-      <p>{num1}</p>
-      <p>{num2}</p>
-      <p>{result}</p>
+      <div>
+        <input type="number" onChange={val1} />
+        <br />
+        <input type="number" onChange={val2} /><br /> <br />
+        <button onClick={add => { setResult(num1 + num2) }}>Add</button>
+        <button onClick={sub => { setResult(num1 - num2) }}>Sub</button>
+        <button onClick={mul => { setResult(num1 * num2) }}>Mul</button>
+        <button onClick={div => { setResult(num1 / num2) }}>Divide</button>
+        <p>{num1}</p>
+        <p>{num2}</p>
+        <p>{result}</p>
+      </div>
     </>
   )
 }
