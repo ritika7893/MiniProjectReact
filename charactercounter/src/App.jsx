0011ed726a9f2551d5ae37ed26e563e71b152617
ritika7893 <ritikakumari87344@gmail.com>
2025-07-21 10:16:17 +0530

    Optimized charcater counter

--- charactercounter/src/App.jsx
+++ charactercounter/src/App.jsx
@@ -1,22 +1,39 @@
 import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useEffect } from 'react'
+import { useRef } from 'react'
 
 function App() {
   const [text, setText] = useState('')
-  const [length, setLength] = useState(0)
-  useEffect(() => {
-    setLength(text.length)
-    if (length >= 100) {
-      alert("Word Limit reached")
+  const hasAlerted = useRef(false)
+  const [reminder, setReminder] = useState('')
+  const handler = (e) => {
+    const input = e.target.value;
+
+    if (input.length > 100) {
+
+      if (!hasAlerted.current) {
+        hasAlerted.current = true
+        setReminder("word limit exceeds")
+      }
+      return
     }
-  }, [text])
+    else {
+      setText(input);
+      if (hasAlerted.current) {
+        setReminder("")
+        hasAlerted.current = false;
+      }
+    }
+  }
   return (
     <>
-      <input type="text" onChange={(e) => { setText(e.target.value) }} />
+      <input type="text" value={text} onChange={handler} />
       <p>{text.length}</p>
+      <p style={{ color: 'red' }}>{reminder}</p >
     </>
   )
 }
